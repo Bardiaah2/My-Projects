6656d9e8fd9660e3c5d8e25cbb5a5095ba265ae2
Bardia <bardiaah@uoregon.edu>
2023-10-15 14:55:05 -0700

    Completed the triangle, there are still some imporvment to be made and some changes to be applied

--- The_Sierpinski_triangle.py
+++ The_Sierpinski_triangle.py
@@ -1,6 +1,15 @@
+# The Sierpinski triangle
+# Author: Bardia Ahmadi
+# Partner: Got the idea from someone in instagram
+# Description: draws a Sierpinski triangle in Turtle with "random" dots
 import turtle as tr
-from random import randint
+from random import randint, choice
 from math import sqrt
+
+
+def mean(a, b):
+    return (a + b) / 2
+
 
 t = tr.Turtle()
 tr.title("THE SIERPINSKI TRIANGLE")
@@ -49,17 +58,24 @@ vertex0, vertex1, vertex2 = triangle(t, 600)
 
 # Locate a point in the triangle
 # Problem: the probability of coordination in the triangle are not the same
-# Problem: we probably don't want dots on around the middle of the triangle?
-init_y = randint(int(vertex2[0]), int(vertex1[0]))
-# init_x =
-print(vertex0, vertex1, vertex2, init_y)
+# Problem: we probably want dots on around the middle of the triangle?
+init_x = randint(int(vertex2[0]), int(vertex1[0]))
+max_y = vertex0[1] + (vertex1[1] - vertex0[1]) / (vertex1[0]) * abs(init_x)
+init_y = randint(int(vertex1[1]), int(max_y))
+
+t.pu()
+t.goto(init_x, init_y)
 
 
 # Randomly choose a vertex and move the turtle there, stamp
 # repeat
-
-
+t.speed(0)
+t.shapesize(0.1)
 t.stamp()
-t.fd(50)
+for i in range(10000):
+    vertex = choice((vertex0, vertex1, vertex2))
+    t.goto(mean(t.xcor(), vertex[0]), mean(t.ycor(), vertex[1]))
+    t.stamp()
 
+print("finish")
 tr.mainloop()
